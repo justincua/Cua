b1aaf24de5d60849c5be9132b9206af99b31ef00
justincua <96491819+justincua@users.noreply.github.com>
2024-04-01 12:07:02 +0800

    Update CuaBuffView.sh

--- CuaBuffView.sh
+++ CuaBuffView.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Kiểm tra xem có liên kết được cung cấp không
+# Kiểm tra xem có đường dẫn đến video YouTube được cung cấp không
 if [ -z "$1" ]; then
-    echo "Dữ Liệu: $0 <YouTube_URL>"
+    echo "Usage: $0 <YouTube_URL>"
     exit 1
 fi
 
 # Sử dụng youtube-dl để tìm và phát video từ liên kết
 url="$1"
-video_url=$(youtube-dl -g "$url")
+video_url=$(youtube-dl --get-url --skip-download "$url")
 
 # Kiểm tra nếu không tìm thấy đường dẫn video
 if [ -z "$video_url" ]; then
